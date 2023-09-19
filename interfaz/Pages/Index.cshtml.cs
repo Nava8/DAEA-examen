f5199bbe78be8b24f1bf0ca77e38994dc4669383
Nava8 <sebastian.navarro@tecsyo.edu.pe>
2023-09-18 20:57:59 -0500

    .

--- interfaz/Pages/Index.cshtml.cs
+++ interfaz/Pages/Index.cshtml.cs
@@ -28,37 +28,34 @@ namespace interfaz.Pages
             {
                 var response = await client.GetStringAsync(apiUrl + Query);
 
-                // Deserializar la respuesta JSON usando System.Text.Json
                 var apiData = JsonSerializer.Deserialize<ApiResponseModel>(response, new JsonSerializerOptions
                 {
-                    PropertyNameCaseInsensitive = true // Esto permite que coincida con las propiedades en minúsculas en el JSON
+                    PropertyNameCaseInsensitive = true 
                 });
 
-                // Verificar si hay resultados y que el primer resultado tenga un GIF
-                // Verificar si hay resultados y que el primer resultado tenga un GIF
                 if (apiData?.results != null && apiData.results.Count > 0 && apiData.results[0]?.media_formats?.gif?.url != null)
                 {
                     var gifUrl = apiData.results[0].media_formats.gif.url;
-                    var gifDescription = apiData.results[0].content_description;
+                   
                     
-                    // Establecer la URL del GIF y la descripción en ViewData
+                    
                     ViewData["GifUrl"] = gifUrl;
-                    ViewData["Description"] = gifDescription;
+                   
                 }
                 else
                 {
-                    // Manejar el caso en el que no se encuentra un GIF
+                   
                     ViewData["Error"] = "No se encontró un GIF en la respuesta.";
                 }
 
             }
             catch (HttpRequestException ex)
             {
-                // Puedes manejar los errores aquí y mostrar un mensaje de error en la vista
+                
                 ViewData["Error"] = $"Error al consultar la API: {ex.Message}";
             }
 
-            // Redirigir de nuevo a la misma página
+            
             return Page();
         }
 
@@ -75,12 +72,6 @@ public class ApiResponseModel
 public class ResultModel
 {
     public MediaFormatsModel media_formats { get; set; }
-    public string content_description { get; set; }
-}
-
-public class ContentDescriptionModel
-{
-    public string content_description { get; set;}
 }
 
 public class MediaFormatsModel
